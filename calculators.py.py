3de96a5c4ab19832f5d2b0b0927c025c78e6b32b
rainisharaisbps-dotcom <rainisharaisbps@gmail.com>
2025-08-10 17:59:15 +0530

    Add files via upload

---
+++ calculators.py.py
@@ -0,0 +1,7 @@
+# making  a calculators 
+first = int(input("Enter yor first number"))
+second = int(input("Enter yor second number"))
+print("The sum is",firt + second )
+print("the difference is ", first - second)
+print("the product is " , first * second)
+print("the division is ", first / second)
---
+++ task 2.py
@@ -0,0 +1,4 @@
+# Auto matic greeting to users
+name = input("enter your name :")
+surname = input("enter you surname")
+print("hello ", name , surname "welcome to the world of programmers")
